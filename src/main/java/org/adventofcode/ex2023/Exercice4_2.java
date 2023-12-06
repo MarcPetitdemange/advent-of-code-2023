90be911b1af02229135956e52f2f608fa85d6090
Marc Petitdemange <marc.petitdemange57@gmail.com>
2023-12-06 23:31:22 +0100

    modif ex 4.2 fonctionnel

--- src/main/java/org/adventofcode/ex2023/Exercice4_2.java
+++ src/main/java/org/adventofcode/ex2023/Exercice4_2.java
@@ -12,18 +12,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Exercice4_2 {
-    
-    
+
+
+    public static final int NB_LINE = 218;
+
     public static void main(String[] args) throws Exception {
         ArrayList<Integer> gameResults = new ArrayList();
         
-        InputStream resource = Exercice2_1.class.getResourceAsStream("/Exercice_4_text.txt");
+        InputStream resource = Exercice2_1.class.getResourceAsStream("/Exercice_4.txt");
         BufferedReader bf = new BufferedReader(new InputStreamReader(Objects.requireNonNull(resource)));
         String line;
         
         HashMap<Integer, Integer> countInstanceForEachGame = new HashMap<Integer, Integer>();
         
-        for(int i = 1; i <= 218;i++){
+        for(int i = 1; i <= NB_LINE; i++){
             countInstanceForEachGame.putIfAbsent(i, 1);
         }
         
@@ -31,10 +33,10 @@ public class Exercice4_2 {
             String regexAftl = "\\| (?<afterLane>(\\d+|\\s)+)$";
             analyzeGame(line, countInstanceForEachGame);
         }
-        
-        
+
+        System.out.println("List count instance : " + countInstanceForEachGame.values());
         System.out.println("List count instance : " + countInstanceForEachGame.values().stream().reduce(Integer::sum));
-        
+
     }
     
     private static void analyzeGame(String line, HashMap<Integer, Integer> countInstanceForEachGame) {
@@ -53,8 +55,8 @@ public class Exercice4_2 {
         matcherAftl.find();
         String beforeLane = matcherBfl.group("beforeLane").trim().replaceAll(" +", " ");
         String afterLane = matcherAftl.group("afterLane").trim().replaceAll(" +", " ");
-   
-        
+
+
         List<Integer> bflInts = Arrays.stream(beforeLane.split(" +")).map(Integer::parseInt).toList();
         List<Integer> aftlInts = Arrays.stream(afterLane.split(" +")).map(Integer::parseInt).toList();
         
@@ -67,12 +69,10 @@ public class Exercice4_2 {
 //        System.out.println("matchings " + matchings);
         
         System.out.println("Refresh for card n° " + noCard + " : matching " + matchings.size());
-        for(int i = noCard; i <= matchings.size(); i++){
+        for(int i = 1; (i <= matchings.size()); i++){
             int noNextCard = noCard + i;
-            if(i < 219){
-                countInstanceForEachGame.compute(noNextCard, (k,v) -> v * 2);
-                System.out.println("n°" + noNextCard + " --> " + countInstanceForEachGame.get(noNextCard) );
-            }
+            countInstanceForEachGame.compute(noNextCard, (k,v) -> v + countInstanceForEachGame.get(noCard));
+            System.out.println("n°" + noNextCard + " --> " + countInstanceForEachGame.get(noNextCard) );
         }
     }
     
